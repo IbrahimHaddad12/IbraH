5f3e412cab767baed10878b61b47b9691bb93a51
AOC-2-2003 <ibrahimhadad@aocol.net>
2021-10-22 09:38:34 +0300

    Grid+Bombs

--- app/src/main/java/com/example/ibrah/GameEngine.java
+++ app/src/main/java/com/example/ibrah/GameEngine.java
@@ -2,9 +2,16 @@ package com.example.ibrah;
 
 import android.content.Context;
 
+import com.example.ibrah.util.Generator;
+import com.example.ibrah.util.PrintGrid;
+
 public class GameEngine {
 
     private static GameEngine instance;
+
+    private static final int BOMB_NUMBER = 10;
+    private static final int WIDTH = 10;
+    private static final int HEIGHT = 10;
 
     private Context context;
 
@@ -27,7 +34,8 @@ public class GameEngine {
         this.context = context;
 
         //create the grid and store it//
-
+        int[][]GeneratedGrid = Generator.generate(BOMB_NUMBER, WIDTH, HEIGHT);
+        PrintGrid.print(GeneratedGrid, WIDTH, HEIGHT);
     }
 
 }
