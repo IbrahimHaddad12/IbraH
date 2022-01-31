a92a0831f93dbb3a895a5e083e3609fac034f49f
AOC-2-2003 <ibrahimhadad@aocol.net>
2022-01-31 13:01:14 +0200

    Image Slider LEurope

--- app/src/main/java/com/example/ibrah/LEuropeDesc.java
+++ app/src/main/java/com/example/ibrah/LEuropeDesc.java
@@ -2,13 +2,44 @@ package com.example.ibrah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.number.Scale;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ViewSwitcher;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+
+import java.util.ArrayList;
+
 public class LEuropeDesc extends AppCompatActivity {
+
+    private ImageSlider ImageSliderLEurope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leurope_desc);
+
+        ImageSliderLEurope = findViewById(R.id.ImageSliderLEurope);
+
+        ArrayList<SlideModel>slideModels = new ArrayList<>();
+
+        slideModels.add(new SlideModel("https://www.aman.com/sites/default/files/styles/masonry_extra_large/public/2021-03/Amanzoe-Gallery-1.jpg?itok=8Ka85Vdg", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://www.aman.com/sites/default/files/styles/masonry_extra_large/public/2021-03/Amanzoe_Gallery_2.jpg?itok=4as4M02m", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://www.aman.com/sites/default/files/styles/masonry_extra_large/public/2021-03/Amanzoe-Greece-9.jpg?itok=pBW5th3P", ScaleTypes.FIT));
+        slideModels.add(new SlideModel("https://www.aman.com/sites/default/files/styles/masonry_extra_large/public/2021-03/Amanzoe_Gallery_7.jpg?itok=-qpYI1dF", ScaleTypes.FIT));
+
+        ImageSliderLEurope.setImageList(slideModels, ScaleTypes.FIT);
+
     }
 }
